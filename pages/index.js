9c4d4193795179d63cc659210ca0b614fb2156b2
Oleg <oleg17.k@gmail.com>
2023-05-09 19:27:40 +0300

    getting data of posts from a markdown file

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,25 @@
 import { Fragment } from "react";
-import Hero from "./components/home-page/hero";
+import Hero from "../components/home-page/hero";
+import FeaturedPosts from "../components/home-page/featured-posts";
+import { getfeaturedPosts } from "../lib/posts-util";
 
-const HomePage = () => {
+const HomePage = (props) => {
   return (
     <Fragment>
       <Hero />
+      <FeaturedPosts posts={props.posts} />
     </Fragment>
   );
+};
+
+export const getStaticProps = () => {
+  const featuredPosts = getfeaturedPosts();
+
+  return {
+    props: {
+      posts: featuredPosts,
+    },
+  };
 };
 
 export default HomePage;
